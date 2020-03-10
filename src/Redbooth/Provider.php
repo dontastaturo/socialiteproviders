198b8ec2a493e6117b33b23faf0a662d0ae6e145
Ralf Siebert <r.siebert@mmc-agentur.de>
2020-03-10 11:26:00 +0100

    refresh access token

--- src/Redbooth/Provider.php
+++ src/Redbooth/Provider.php
@@ -4,6 +4,7 @@ namespace SocialiteProviders\Redbooth;
 
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
+use GuzzleHttp\ClientInterface;
 
 class Provider extends AbstractProvider
 {
@@ -40,7 +41,7 @@ class Provider extends AbstractProvider
     {
         $response = $this->getHttpClient()->get('https://redbooth.com/api/3/me', [
             'headers' => [
-                'Authorization' => 'Bearer '.$token,
+                'Authorization' => 'Bearer ' . $token,
             ],
         ]);
 
@@ -67,5 +68,25 @@ class Provider extends AbstractProvider
         return array_merge(parent::getTokenFields($code), [
             'grant_type' => 'authorization_code',
         ]);
+    }
+
+    public function refreshAccessToken($refresh_token)
+    {
+        $postKey = (version_compare(ClientInterface::VERSION, '6') === 1) ? 'form_params' : 'body';
+
+        $response = $this->getHttpClient()->post(
+            $this->getTokenUrl(),
+            [
+                'headers' => ['Accept' => 'application/json'],
+                $postKey => [
+                    'client_id' => $this->clientId,
+                    'client_secret' => $this->clientSecret,
+                    'refresh_token' => $refresh_token,
+                    'grant_type' => 'refresh_token'
+                ],
+            ]
+        );
+
+        return json_decode($response->getBody(), true);
     }
 }
